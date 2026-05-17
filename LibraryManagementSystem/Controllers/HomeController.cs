c7c218975e4450bb3f398a28d66d19909844c4de
20032376 <20032376@students.koi.edu.au>
2026-05-17 19:51:21 +1000

    Revert "mvc-auth-setup"

--- LibraryManagementSystem/Controllers/HomeController.cs
+++ LibraryManagementSystem/Controllers/HomeController.cs
@@ -1,59 +1,30 @@
-// =============================================================================
-// HomeController.cs - Home & Public Pages Controller
-// Library Management System
-// =============================================================================
-// Handles the public-facing pages of the application:
-//   - Home page (library introduction)
-//   - Error page
-// These pages are accessible without authentication.
-// =============================================================================
-
-using LibraryManagementSystem.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace LibraryManagementSystem.Controllers
 {
-    /// <summary>
-    /// Controller for public pages accessible to all visitors.
-    /// </summary>
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        /// <summary>
-        /// GET: /Home/Index
-        /// Displays the library home page with introduction and features.
-        /// </summary>
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
-        /// <summary>
-        /// GET: /Home/About
-        /// Displays information about the library system.
-        /// </summary>
-        public IActionResult About()
+        public ActionResult About()
         {
+            ViewBag.Message = "Your application description page.";
+
             return View();
         }
 
-        /// <summary>
-        /// Displays the error page with request details.
-        /// </summary>
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public ActionResult Contact()
         {
-            return View(new ErrorViewModel
-            {
-                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-            });
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
     }
 }
